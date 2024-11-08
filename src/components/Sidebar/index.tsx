f3398fdb6603e69aea40d211cc6bb1592f75ef48
Victtor777 <victtor.eng.software@gmail.com>
2024-11-08 17:44:55 -0300

    Refactor Sidebar component and add MainNavigation with navigation items

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,13 @@
 import { Search } from "lucide-react";
 import { Logo } from "./logo";
+import { MainNavigation } from "./MainNavigation";
 
 export function Sidebar() {
   return (
     <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+      <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="w-5 h-5 text-zinc-500" />
         <input
           type="text"
@@ -14,6 +15,8 @@ export function Sidebar() {
           placeholder="Search"
         />
       </div>
+
+      <MainNavigation />
     </aside>
   );
 }
